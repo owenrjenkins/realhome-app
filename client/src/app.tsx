ff0c99ba8524bc2e30cd7c7816162275ef42f3ff
owenrjenkins <owenrjenkins@gmail.com>
2025-09-04 16:14:28 +0100

    Create app.tsx

---
+++ client/src/app.tsx
@@ -0,0 +1,9 @@
+import TravelFilteredMap from "./components/TravelFilteredMap";
+
+export default function App() {
+  return (
+    <div style={{ width: "100vw", height: "100vh" }}>
+      <TravelFilteredMap />
+    </div>
+  );
+}
